94dfcb7d1fff0ae1321ee39f63a6d2db44c6f7a9
pash masr <129723189+pashamasr01287654800@users.noreply.github.com>
2025-09-13 06:57:20 +0300

    Add files via upload

--- install-msfvenom-completion_bash.sh
+++ install-msfvenom-completion_bash.sh
@@ -82,7 +82,7 @@ _msfvenom_completion() {
     fi
 }
 
-msfupdata-completion-update() {
+msfvenom-completion-update() {
     echo "[*] Updating msfvenom autocomplete cache..."
     _build_cache
     echo "[+] Cache updated in $CACHE_DIR"
@@ -93,5 +93,5 @@ complete -F _msfvenom_completion msfvenom
 EOF
 
 echo "[*] Installing/Updating msfvenom-completion..."
-bash -c "source $INSTALL_PATH; msfupdata-completion-update"
+bash -c "source $INSTALL_PATH; msfvenom-completion-update"
 echo "[+] Installation complete. Restart your shell to enable autocomplete."
